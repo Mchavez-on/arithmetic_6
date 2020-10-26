16d9bc20bbf2e6c4e5088252867d31c8421a8852
Mariana Chavez <57697974+Mchavez-on@users.noreply.github.com>
2020-10-26 13:27:59 -0700

    Update 6.py

--- 6.py
+++ 6.py
@@ -3,27 +3,17 @@
 # get two numbers from user
 num_1 = int(input("Enter a number!"))
 num_2 = int(input("Enter another number!")) 
-def get_user_numbers():
-  num_1 = int(input("Enter a number!"))
-  num_2 = int(input("Enter another number!"))
 
-# loop whle the numbers are not divisible (the remainder is 0)
-rem = num_1 % num_2
-def divide_user_numbers():
-  rem = num_1 % num_2 
-
-
-while (rem != 0):
+while (num_1 % num_2 != 0):
   # inform user of result 
   print("The two numbers you selected do not divide to zero, try again!")
-  get_user_numbers()
-  divide_user_numbers()
-  if (rem == 0):
-    break
-print("Congratulations! the numbers you have selected divide into zero.")
-  
-  
-  
   # gather user input again
+  num_1 = int(input("Enter a number!"))
+  num_2 = int(input("Enter another number!")) 
+  # inform user of result 
+  print("Congratulations! the numbers", num_1, "and", num_2, "you have selected divide into zero.")
   
-# inform user of result 
+  
+  
+
+
